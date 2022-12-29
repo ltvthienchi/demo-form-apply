60a03ede857ecba45d53e10d710de71ba0b9dc02
william_to <chi.to@zillearn.com>
2022-12-29 18:33:24 +0700

    Init code

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/demo-2'),
+      dir: require('path').join(__dirname, './coverage/qna-exam'),
       subdir: '.',
       reporters: [
         { type: 'html' },
